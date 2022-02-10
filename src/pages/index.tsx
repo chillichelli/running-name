aea8b2c9cbdaa02dbc902b341d2a37ac97445e66
Nigel Janssens <janssensnigel@gmail.com>
2022-02-10 17:32:25 +0100

    redesign landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,12 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className='max-w-screen-2xl mx-auto'>
+    <div className="max-w-screen-2xl mx-auto">
       <Head>
         <title>Skyscanner 2.0</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
+      {/* <Header /> */}
       <Hero />
     </div>
   );
